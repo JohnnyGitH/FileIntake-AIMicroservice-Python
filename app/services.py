b79cc22eaa82abbb3b854d62a365ea231c84ddfb
Johnny <joncockrem@gmail.com>
2025-12-02 08:51:42 -0500

    Finishing up the service, modified for testing before actual OpenAI use

--- app/services.py
+++ app/services.py
@@ -1,11 +1,23 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("Key_Goes_here"))
 
-async def summarize_document(text:str) -> str:
-    response = client.chat.completions.create(
-        model="gpt-4o-mini" #Figure out the cheapest one to use
-        messages=[{"role":"user","content": f"Summarize this: {text}"}]
-    )
-    return response.choices[0].message["content"]
+async def summarize_document(prompt:str) -> str:
+    # response = client.chat.completions.create(
+    #     model="gpt-4o-mini", #Figure out the cheapest one to use
+    #     messages=[
+    #         {"role":"system","content": "You are a helpful assistant"}, # Context
+    #         {"role":"user","content": f"Summarize this: {prompt}"} # prompt
+    #     ],
+    # )
+
+    # reply = response.choices[0].message.content
+    # print(reply)
+
+    # return reply or ""
+
+    if not prompt:
+        return "No Content Available"
+    
+    return "This is a sample AI response. The Service is connected BEEP BOOP"
